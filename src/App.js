7ecc723c1af0b84493b11a134b33a7ef2eb185bb
harshi0224 <kunkuharshitha@gmail.com>
2023-09-09 11:43:10 +0530

    shopping2

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import Shopping from './Shopping';
+import Shopping2 from './Shopping2';
+import React from 'react';
 function App() {
   return (
     <div>
-      <Shopping></Shopping>
+      <Shopping2></Shopping2>
     </div>
   );
 }
